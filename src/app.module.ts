cd328d78861428d77bfe888ee111c363d18dcc7e
g-yunjh <yunjh1126@naver.com>
2025-08-21 23:51:48 +0900

    push

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,6 +3,7 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { UserModule } from './user/user.module';
+import { User } from './user/entities/user.entity';
 
 @Module({
   imports: [
@@ -13,7 +14,7 @@ import { UserModule } from './user/user.module';
       username: 'yunjh',
       password: 'a.103903',
       database: 'exPg',
-      entities: [],
+      entities: [User],
       synchronize: true,
       logging: true,
     }),
